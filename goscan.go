a8609aae031b6ffb9b9382dc5894b7f3c1876eaf
3santree <wangyueffect@163.com>
2022-09-28 11:02:44 -0400

    fmt code

--- goscan.go
+++ goscan.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"moul.io/banner"
 	"net"
-	"strconv"
-	"time"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -26,42 +26,43 @@ func main() {
 	open_ports_channel := make(chan int, 1000)
 	var open_ports []int
 	var wg sync.WaitGroup
-	
-	
+
 	//1000 workers
-	
-	for i:= 1; i <= 1000; i++ {
+
+	for i := 1; i <= 1000; i++ {
 		wg.Add(1)
-		
+
 		go func() {
 			defer wg.Done()
 			scanner(i, ip, ports, open_ports_channel)
 		}()
 	}
-	
+
 	//Sending jobs
-	
-	for i:=1; i<= number_of_ports; i++ {
+
+	for i := 1; i <= number_of_ports; i++ {
 		ports <- i
 	}
 	close(ports)
 	wg.Wait()
+
 	// Will hang if not close
 	close(open_ports_channel)
+	
 	//Getting open ports
 	for open_port := range open_ports_channel {
 		open_ports = append(open_ports, open_port)
-	}	
+	}
+	
 	//fmt.Println(strings.Trim(strings.Join(strings.Fields(fmt.Sprint(open_ports)), ","), "[]"))
 	nmap(ip, open_ports)
 }
 
-
-func scanner(id int, ip string, ports <- chan int, open_ports chan int) {
+func scanner(id int, ip string, ports <-chan int, open_ports chan int) {
 	for port := range ports {
 		address := ip + ":" + strconv.Itoa(port)
-		for i:=1; i<=2; i++ {
-			_, err := net.DialTimeout("tcp", address, time.Millisecond * 200)
+		for i := 1; i <= 2; i++ {
+			_, err := net.DialTimeout("tcp", address, time.Millisecond*200)
 			if err == nil {
 				fmt.Println("Open ", port)
 				open_ports <- port
@@ -87,6 +88,6 @@ func nmap(ip string, open_ports []int) {
 	fmt.Println(output)
 }
 
-func print_banner(){
+func print_banner() {
 	fmt.Println(banner.Inline("goGun"))
 }
